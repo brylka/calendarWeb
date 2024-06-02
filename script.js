8a55ebfec802deae95ee4ee657be89dce1b27dec
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-02 14:06:59 +0200

    Converting the condition to a ternary operator

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
     for (let i = 1; i <= 31; i++) {
-        if (i % 7 == 1) { // Każdy pierwszy dzień tygodnia zaczyna nowy rząd
-            $("#calendar").append('<div class="clear">' + i + '</div>');
-        } else {
-            $("#calendar").append("<div>" + i + "</div>");
-        }
+        //if (i % 7 == 1) { // Każdy pierwszy dzień tygodnia zaczyna nowy rząd
+        //    $("#calendar").append('<div class="clear">' + i + '</div>');
+        //} else {
+        //    $("#calendar").append("<div>" + i + "</div>");
+        //}
+        $("#calendar").append('<div class="' + ((i % 7 == 1) ? 'clear' : '') + '">' + i + '</div>');
     }
 });
