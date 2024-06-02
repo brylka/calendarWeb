e2b201ae211a796657744f9931c08c68a892968e
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-02 14:14:30 +0200

    Refactor code using jQuery class management methods

--- script.js
+++ script.js
@@ -4,11 +4,21 @@ $(document).ready(function() {
 
     // Wyświetlenie nazw dni tygodnia z odpowiednią klasą dla weekendu
     for (let i = 0; i <= 6; i++) {
-        $("#calendar").append('<div class="day-name' + (i == 5 ? ' saturday' : i == 6 ? ' sunday' : '') + '">' + dayName[i] + '</div>');
+        //$("#calendar").append('<div class="day-name' + (i == 5 ? ' saturday' : i == 6 ? ' sunday' : '') + '">' + dayName[i] + '</div>');
+        var divDay = $('<div>').text(dayName[i]).addClass('day-name');
+        if (i == 5) { divDay.addClass('saturday'); }
+        if (i == 6) { divDay.addClass('sunday'); }
+        $("#calendar").append(divDay);
     }
 
     // Wyświetlenie dni miesiąca z odpowiednią klasą dla weekendu
     for (let i = 1; i <= 31; i++) {
-        $("#calendar").append('<div class="' + ((i % 7 == 1) ? 'clear' : (i % 7 == 0) ? 'sunday' : (i % 7 == 6) ? 'saturday' : '') + '">' + i + '</div>');
+        //$("#calendar").append('<div class="' + ((i % 7 == 1) ? 'clear' : (i % 7 == 0) ? 'sunday' : (i % 7 == 6) ? 'saturday' : '') + '">' + i + '</div>');
+        var divDay = $('<div>').text(i);
+        // Dodanie klasy w zależności od dnia tygodnia
+        if (i % 7 == 1) { divDay.addClass('clear'); }
+        if (i % 7 == 0) { divDay.addClass('sunday'); }
+        if (i % 7 == 6) { divDay.addClass('saturday'); }
+        $("#calendar").append(divDay);
     }
 });
