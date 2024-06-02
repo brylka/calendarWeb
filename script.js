122eeb667d474312a0dac965d62f3f00b8e57c6e
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-02 17:04:27 +0200

    Added navigation between months in the calendar

--- script.js
+++ script.js
@@ -3,56 +3,78 @@ $(document).ready(function() {
     var date = new Date();
     var month = date.getMonth() + 1; // Pobranie aktualnego miesiąca i korekta indeksu (getMonth zwraca miesiące od 0 do 11)
     var year = date.getFullYear(); // Pobranie aktualnego roku w formacie czterocyfrowym
-    var day = date.getDate(); // Pobranie aktualnego dnia miesiąca
+    generateCalendar(month, year);
 
-    // Obliczenie pierwszego dnia miesiąca
-    var startDay = new Date(year, month - 1, 1).getDay(); // .getDay() zwraca indeksy dni tygodnia 0-6, gdzie 0 to niedziela
-    if (startDay === 0) { startDay = 7; } // Zamiana numeru dla niedzieli na format 1-7
+    function generateCalendar(month, year) {
+        var day = date.getDate(); // Pobranie aktualnego dnia miesiąca
 
-    // Pobranie maksymalnego dnia w miesiącu
-    var maxDay = new Date(year, month, 0).getDate();
+        // Obliczenie pierwszego dnia miesiąca
+        var startDay = new Date(year, month - 1, 1).getDay(); // .getDay() zwraca indeksy dni tygodnia 0-6, gdzie 0 to niedziela
+        if (startDay === 0) { startDay = 7; } // Zamiana numeru dla niedzieli na format 1-7
 
-    // Tablica z nazwami miesięcy
-    var monthName = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
+        // Pobranie maksymalnego dnia w miesiącu
+        var maxDay = new Date(year, month, 0).getDate();
 
-    // Tworzenie fragmentu dokumentu
-    var fragment = $(document.createDocumentFragment());
+        // Tablica z nazwami miesięcy
+        var monthName = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"];
 
-    // Wyświetlanie nazwy miesiąca i roku oraz elementów nawigacyjnych
-    fragment.append($('<div>').text("<"));
-    fragment.append($('<div>').text(monthName[month-1] + " " + year).addClass('month-year'));
-    fragment.append($('<div>').text(">"));
+        // Tworzenie fragmentu dokumentu
+        var fragment = $(document.createDocumentFragment());
 
-    // Tablica z nazwami dni tygodnia
-    var dayName = ["Pon", "Wto", "Śro", "Czw", "Pią", "Sob", "Nie"];
+        // Wyświetlanie nazwy miesiąca i roku oraz elementów nawigacyjnych
+        fragment.append($('<div>').text("<").addClass('nav prev'));
+        fragment.append($('<div>').text(monthName[month - 1] + " " + year).addClass('month-year'));
+        fragment.append($('<div>').text(">").addClass('nav next'));
 
-    // Wyświetlenie nazw dni tygodnia z odpowiednią klasą dla weekendu
-    for (let i = 0; i <= 6; i++) {
-        divDay = $('<div>').text(dayName[i]).addClass('day-name');
-        if (i === 5) { divDay.addClass('saturday'); }
-        if (i === 6) { divDay.addClass('sunday'); }
-        fragment.append(divDay);
-    }
+        // Tablica z nazwami dni tygodnia
+        var dayName = ["Pon", "Wto", "Śro", "Czw", "Pią", "Sob", "Nie"];
 
-    // Wyświetlenie pustych dni na początku miesiąca
-    for (let i = 1; i < startDay; i++) {
-        divDay = $('<div>').addClass('none');
-        fragment.append(divDay);
-    }
-
-    // Wyświetlenie dni miesiąca
-    for (let i = 1; i <= maxDay; i++) {
-        var ii = i + startDay - 1;
-        divDay = $('<div>').text(i);
-        if (i === day) {
-            divDay.addClass('today');
-        } else {
-            if (ii % 7 === 0) { divDay.addClass('sunday'); }
-            if (ii % 7 === 6) { divDay.addClass('saturday'); }
+        // Wyświetlenie nazw dni tygodnia z odpowiednią klasą dla weekendu
+        for (let i = 0; i <= 6; i++) {
+            divDay = $('<div>').text(dayName[i]).addClass('day-name');
+            if (i === 5) { divDay.addClass('saturday'); }
+            if (i === 6) { divDay.addClass('sunday'); }
+            fragment.append(divDay);
         }
-        fragment.append(divDay);
+
+        // Wyświetlenie pustych dni na początku miesiąca
+        for (let i = 1; i < startDay; i++) {
+            divDay = $('<div>').addClass('none');
+            fragment.append(divDay);
+        }
+
+        // Wyświetlenie dni miesiąca
+        for (let i = 1; i <= maxDay; i++) {
+            var ii = i + startDay - 1;
+            divDay = $('<div>').text(i);
+            if (i === day) {
+                divDay.addClass('today');
+            } else {
+                if (ii % 7 === 0) { divDay.addClass('sunday'); }
+                if (ii % 7 === 6) { divDay.addClass('saturday'); }
+            }
+            fragment.append(divDay);
+        }
+
+        // Dodanie całego fragmentu do elementu #calendar
+        $("#calendar").empty().append(fragment);
     }
 
-    // Dodanie całego fragmentu do elementu #calendar
-    $("#calendar").append(fragment);
+    // Onclick dla przycisków nawigujących kalendarzem
+    $(document).on('click', '.nav', function() {
+        if ($(this).hasClass('prev')) {
+            month--;
+            if (month < 1) {
+                month = 12;
+                year--;
+            }
+        } else if ($(this).hasClass('next')) {
+            month++;
+            if (month > 12) {
+                month = 1;
+                year++;
+            }
+        }
+        generateCalendar(month, year);
+    });
 });
