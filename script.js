bc468589f99124e88f552a36925f44e373b0c99b
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-02 14:11:58 +0200

    Apply styles for weekends in the calendar

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     // Tablica z nazwami dni tygodnia
     var dayName = ["Pon", "Wto", "Śro", "Czw", "Pią", "Sob", "Nie"];
 
-    // Wyświetlenie nazw dni tygodnia
+    // Wyświetlenie nazw dni tygodnia z odpowiednią klasą dla weekendu
     for (let i = 0; i <= 6; i++) {
-        $("#calendar").append('<div class="day-name">' + dayName[i] + '</div>');
+        $("#calendar").append('<div class="day-name' + (i == 5 ? ' saturday' : i == 6 ? ' sunday' : '') + '">' + dayName[i] + '</div>');
     }
 
-    // Wyświetlenie dni miesiąca
+    // Wyświetlenie dni miesiąca z odpowiednią klasą dla weekendu
     for (let i = 1; i <= 31; i++) {
-        $("#calendar").append('<div class="' + ((i % 7 == 1) ? 'clear' : '') + '">' + i + '</div>');
+        $("#calendar").append('<div class="' + ((i % 7 == 1) ? 'clear' : (i % 7 == 0) ? 'sunday' : (i % 7 == 6) ? 'saturday' : '') + '">' + i + '</div>');
     }
 });
