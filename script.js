9fae63883435432acf08a39c7f02ac64af39f401
Bartosz Bryniarski <brylka@jelenia.com>
2024-06-02 13:57:42 +0200

    Adding a transition to a new line after 7 days

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     for (let i = 1; i <= 31; i++) {
-        $("#calendar").append("<div>" + i + "</div>");
+        if (i % 7 == 1) { // Każdy pierwszy dzień tygodnia zaczyna nowy rząd
+            $("#calendar").append('<div class="clear">' + i + '</div>');
+        } else {
+            $("#calendar").append("<div>" + i + "</div>");
+        }
     }
 });
